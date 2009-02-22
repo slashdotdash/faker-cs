8e511c5164c3a3af345dd0a95a9b0b98fb0b073c
unknown <ben@slashdotdash.net>
2009-02-22 14:55:22 +0000

    Adding Name generator.

---
+++ src/Faker/Extensions/ArrayExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Faker.Extensions
+{
+    public static class ArrayExtensions
+    {
+        private static readonly Random _rnd = new Random();
+
+        /// <summary>
+        /// Select a random element from the IEnumerable list.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static string Random(this string[] list)
+        {
+            if (list.Count() == 0) throw new InvalidOperationException("Array must contain at least one item");
+
+            return list[_rnd.Next(list.Length - 1)];
+        }
+    }
+}
---
+++ tests/Faker.Tests/Extensions/ArrayExtensionsFixture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Faker.Tests.Extensions
+{
+    public class ArrayExtensionsFixture
+    {
+        //[Test]
+        //public void Should
+    }
+}
