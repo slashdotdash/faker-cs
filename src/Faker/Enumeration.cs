0db783b1bfb9c2e32af378fbadb669d4b3ac3aec
Adrian <suvroc@o2.pl>
2015-04-28 23:30:42 +0200

    Enum generator

---
+++ src/Faker/Enumeration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Faker.Extensions;
+
+namespace Faker
+{
+
+    public static class Enumeration
+    {
+
+        /// <summary>
+        /// Choose a random enum.
+        /// </summary>     
+        public static T Random<T>()
+            where T : struct, IConvertible
+        {
+            var values = Enum.GetValues(typeof(T));
+            return (T)values
+                .GetValue(RandomNumber.Next(values.Length));
+        }
+    }
+}
