efc6c215bea675c4bb842e198f4d5528cd284045
Peter Gill <peter.gill@townsuite.com>
2016-09-26 11:49:58 -0230

    event names

---
+++ tests/Faker.Tests/EventsFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace Faker.Tests
+{
+    [TestFixture]
+    public class EventsFixture
+    {
+        [Test]
+        public void Should_Generate_Name()
+        {
+            var name = Events.Name();
+
+            // Name should match one of the given formats
+            Assert.IsTrue(new List<Func<bool>>
+                              {
+                                  () => Regex.IsMatch(name, @"\w+ \w+"),
+                                  () => Regex.IsMatch(name, @"\w+-\w+"),
+                              }.Any(x => x.Invoke()));
+        }
+
+    }
+}
